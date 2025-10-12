721bf892e2bf897efcbf6772b052958990af2649
Phuc710 <athanhphuc7102005@gmail.com>
2025-10-12 19:29:05 +0700

    Remove failed Lavalink nodes - keep only Serenetia and Amane V4 nodes

--- lavalink.js
+++ lavalink.js
@@ -10,23 +10,9 @@ module.exports = {
         secure: true
       },
       {
-        name: "Amane V4 (EU) - Backup 1",
+        name: "Amane V4 (EU) - Backup",
         password: "https://dsc.gg/ajidevserver",
         host: "lava-v4.ajieblogs.eu.org",
-        port: 443,
-        secure: true
-      },
-      {
-        name: "Muzykant V4 - Backup 2",
-        password: "https://discord.gg/v6sdrD9kPh",
-        host: "lavalink_v4.muzykant.xyz",
-        port: 443,
-        secure: true
-      },
-      {
-        name: "TriniumHost V4 - Backup 3",
-        password: "free",
-        host: "lavalink-v4.triniumhost.com",
         port: 443,
         secure: true
       }
