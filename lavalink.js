5e6e2d6f6824ac382c9f6793bc07f9d91fe0604a
Phuc710 <athanhphuc7102005@gmail.com>
2025-10-12 19:17:14 +0700

    Update Lavalink to use faster V4 nodes (Serenetia, Amane, Muzykant, TriniumHost)

--- lavalink.js
+++ lavalink.js
@@ -3,23 +3,30 @@ module.exports = {
     enabled: true, 
     lavalink: [
       {
-        name: "Boo Bot - Primary Node",
-        password: "youshallnotpass",
-        host: "lavalink.jirayu.net",
+        name: "Serenetia V4 - Primary",
+        password: "https://dsc.gg/ajidevserver",
+        host: "lavalinkv4.serenetia.com",
         port: 443,
         secure: true
       },
       {
-        name: "Boo Bot - Backup Node 1",
-        password: "catfein",
-        host: "lavalink.alfari.id",
+        name: "Amane V4 (EU) - Backup 1",
+        password: "https://dsc.gg/ajidevserver",
+        host: "lava-v4.ajieblogs.eu.org",
         port: 443,
         secure: true
       },
       {
-        name: "Boo Bot - Backup Node 2",
-        password: "catfein",
-        host: "lava.catfein.com",
+        name: "Muzykant V4 - Backup 2",
+        password: "https://discord.gg/v6sdrD9kPh",
+        host: "lavalink_v4.muzykant.xyz",
+        port: 443,
+        secure: true
+      },
+      {
+        name: "TriniumHost V4 - Backup 3",
+        password: "free",
+        host: "lavalink-v4.triniumhost.com",
         port: 443,
         secure: true
       }
