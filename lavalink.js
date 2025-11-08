378a63851a2399dd7689acad6138b21782c96b3f
Speed <154397637+Phuc710@users.noreply.github.com>
2025-11-08 13:19:21 +0700

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -20,41 +20,6 @@ module.exports = {
       host: "lava-v4.ajieblogs.eu.org",
       port: 443,
       secure: true
-    },
-    {
-      name: "Lavalink APGB",
-      password: "youshallnotpass",
-      host: "airplanegobrr.us.to",
-      port: 2333,
-      secure: false
-    },
-    {
-      name: "dctv (sgp)",
-      password: "quangloc2018",
-      host: "s13.oddblox.us",
-      port: 28405,
-      secure: false
-    },
-    {
-      name: "Yumi Team",
-      password: "https://camming.xyz",
-      host: "lavalink.camming.xyz",
-      port: 13592,
-      secure: false
-    },
-    {
-      name: "RRH",
-      password: "RRHosting",
-      host: "uk-01.rrhosting.eu",
-      port: 1337,
-      secure: false
-    },
-    {
-      name: "Free lavalink v3",
-      password: "https://discord.gg/mjS5J2K3ep",
-      host: "107.150.58.122",
-      port: 4006,
-      secure: false
     }
   ]
 };
