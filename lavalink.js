07301bbac3f43ed3cb259dfe6313de76c0946dd9
Speed <154397637+Phuc710@users.noreply.github.com>
2025-11-08 13:16:49 +0700

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -1,21 +1,60 @@
-
 module.exports = {
-    enabled: true, 
-    lavalink: [
-      {
-        name: "Serenetia V4 - Primary",
-        password: "https://dsc.gg/ajidevserver",
-        host: "lavalinkv4.serenetia.com",
-        port: 443,
-        secure: true
-      },
-      {
-        name: "Amane V4 (EU) - Backup",
-        password: "https://dsc.gg/ajidevserver",
-        host: "lava-v4.ajieblogs.eu.org",
-        port: 443,
-        secure: true
-      },
-    ]
+  lavalink: [
+    {
+      name: "1",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 443,
+      secure: true
+    },
+    {
+      name: "3",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 80,
+      secure: false
+    },
+    {
+      name: "2 - Backup",
+      password: "https://dsc.gg/ajidevserver",
+      host: "lava-v4.ajieblogs.eu.org",
+      port: 443,
+      secure: true
+    },
+    {
+      name: "Lavalink APGB",
+      password: "youshallnotpass",
+      host: "airplanegobrr.us.to",
+      port: 2333,
+      secure: false
+    },
+    {
+      name: "dctv (sgp)",
+      password: "quangloc2018",
+      host: "s13.oddblox.us",
+      port: 28405,
+      secure: false
+    },
+    {
+      name: "Yumi Team",
+      password: "https://camming.xyz",
+      host: "lavalink.camming.xyz",
+      port: 13592,
+      secure: false
+    },
+    {
+      name: "RRH",
+      password: "RRHosting",
+      host: "uk-01.rrhosting.eu",
+      port: 1337,
+      secure: false
+    },
+    {
+      name: "Free lavalink v3",
+      password: "https://discord.gg/mjS5J2K3ep",
+      host: "107.150.58.122",
+      port: 4006,
+      secure: false
+    }
+  ]
 };
-
